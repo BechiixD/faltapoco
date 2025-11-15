6c4de2c5adf636c09fc6bbbf24643357d211f69f
Bechi <164094132+BechiixD@users.noreply.github.com>
2025-11-15 12:10:27 -0300

    solve errors

---
+++ src/data/pages.js
@@ -0,0 +1,23 @@
+export const pages = {
+    mundial: {
+        date: {
+            targetIso,
+            toDate,
+        },
+        meta: {
+            pageTitle,
+            description,
+            keywords,
+            url,
+            slug: '',
+            image,
+        },
+        content: {
+            title,
+            conector,
+            whatWhen,
+            left,
+            info
+        }
+    }
+}
